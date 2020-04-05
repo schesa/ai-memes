130bd49a6378974b7d945addd05c8902b1897b23
Sebastian Chesa <sebastian.chesa@berg-software.com>
2020-04-05 18:33:04 +0300

    generate Captions with Boxes

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 require("dotenv").config();
 
+// Image Captioning API basic usage
 axios
   .post(
     "https://api.imgflip.com/caption_image",
@@ -11,13 +12,36 @@ axios
         username: process.env.IMGFLIP_USERNAME,
         password: process.env.IMGFLIP_PASSWORD,
         text0: "Cand ai o pitica",
-        text1: "Dar nu te lasa in pace...."
-      }
+        text1: "Dar nu te lasa in pace....",
+      },
     }
   )
-  .then(res => {
+  .then((res) => {
     console.log(res.data.data);
   })
-  .catch(error => {
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Image Captioning API using boxes
+axios
+  .post(
+    "https://api.imgflip.com/caption_image",
+    {},
+    {
+      params: {
+        template_id: 112126428, // distacted boyfriend meme
+        username: process.env.IMGFLIP_USERNAME,
+        password: process.env.IMGFLIP_PASSWORD,
+        "boxes[0][text]": "Making memes using Ai",
+        "boxes[1][text]": "9GAG Users",
+        "boxes[2][text]": "Making memes",
+      },
+    }
+  )
+  .then((res) => {
+    console.log(res.data.data);
+  })
+  .catch((error) => {
     console.error(error);
   });
